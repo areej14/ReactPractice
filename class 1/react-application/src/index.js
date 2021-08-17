44704bdf8681c5ee2e64914f13659cbf3c1604a7
AREEJ AMJAD <56145161+areej14@users.noreply.github.com>
2021-08-17 13:49:30 +0500

    commit

--- class 1/react-application/src/index.js
+++ class 1/react-application/src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+let a = React.createElement('h1',null,'Hello .. Its areej ')
+let b= React.createElement('p',null,'Class-2 task done')
+let c= React.createElement('li',null,'item 1')
 ReactDOM.render(
+  
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+   <> {a} {b} {c}</>   
+     {/* <App /> */}
+   </React.StrictMode>,
+  
   document.getElementById('root')
 );
 
