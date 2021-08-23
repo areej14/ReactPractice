47830d48505c606ead58557e16eea66e0c8c70d1
AREEJ AMJAD <56145161+areej14@users.noreply.github.com>
2021-08-23 22:17:34 +0500

    Update App.js

--- react-application/src/App.js
+++ react-application/src/App.js
@@ -3,28 +3,10 @@ import "./App.css";
 import pic from "./pic.jpg";
 
 function App() {
-  //Array
-  const subject = [
-    {
-      n: "C++", sem: "1st semester",
-    },
-    { n: "Sys pro", sem: "5th semester" },
-  ];
-  //Object
- const fruit= {
-    fruit1: 'apple',
-    fruit2:'Orange'
-  }
-  fruit.fruit2="Mango"
-  
+ 
   return (
     <>
-      <div className="body">
-      <h1 className="heading">By Areej Amjad</h1>
-      <img className="image" src={pic} alt=" " /><br/>
-      <h2>  {fruit.fruit2} is a fruit.</h2>
-    <b> { subject.map(i => ( i.n + ' studied in ' + i.sem + " "))}</b>
-    </div>
+     
     </>
   );
 }
