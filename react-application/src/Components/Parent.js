dc4c8c891b968d9d60d18c7339acf49087ff08eb
AREEJ AMJAD <56145161+areej14@users.noreply.github.com>
2021-08-30 20:43:39 +0500

    commit

--- react-application/src/Components/Parent.js
+++ react-application/src/Components/Parent.js
@@ -1,38 +1,65 @@
 import React,{useState} from 'react'
 import Child from './Child'
-
 function Parent() {
     const [value, setvalue] = useState(0)
-    const [lock, setlock] = useState(false)
+    const [lock, setlock] = useState(true)
     const [theme, settheme] = useState("black")
+    const [locktext, setlocktext] = useState("Lock")
     const Add=()=>{
         console.log('add');
         (lock)?setvalue(value+1):alert("Counter is in lock mode")
     };
     const Del=()=>{
         console.log('del');
+        if(value>0||value>1){
         (lock)?setvalue(value-1):alert("Counter is in lock mode")
+        }
+        else
+        alert("No is less than 0")
     };
-    const DarkMode=()=>{
-        console.log('theme')
-    };
+    
     const Lock=()=>{
-        console.log('lock')
-        setlock(!lock)
+        if(lock===true){
+        setlock(false)
+        setlocktext("UnLock")
+}
+else{
+    setlock(true)
+        setlocktext("Lock")
+}
+        
     };
-    // const DarkMode=()=>{
-    //     settheme(style={
-    //         backgroundColor:"black",
-    //     })
-    // }
+    const Darkmode=()=>{
+       if  (lock==false){
+          alert("counter is in lock mode")
+       }
+    else{if( theme==='white'){
+        settheme('black')
+        document.body.style.backgroundColor='white';
+        document.body.style.color='black';
+       }
+       else{
+           settheme('white')
+            document.body.style.backgroundColor='black';
+            document.body.style.color='white';
+           }
+       }}
+     
     return (
-        <div>
-            <Child value={value}/>
-            <button onClick={Add}>Add</button>
-            <button onClick={Del}>Delete</button>
-            <button onClick={DarkMode}>DarkMode</button>
-            <button onClick={Lock}>Lock</button>
+        <>
+        
+        <h1 style={{"textAlign":"center"}}><u>COUNTER</u></h1>
+        <div style={{"textAlign":"center","width":"50%","height":"400px", "margin":"10px auto","padding":"10px","borderRadius":"15px", "border":"2px solid grey"}} >
+           
+            <Child  value={value}/>
+            <button class="btn btn-success mx-3" onClick={Add}>Add <i class="fas fa-plus-circle"></i></button>
+            <button  class="btn btn-danger  mx-3" onClick={Del}>Del <i class="fas fa-minus-circle"></i></button>
+            <button  class="btn btn-secondary  mx-3" onClick={Darkmode}>DarkMode <i class="fas fa-adjust"></i></button>
+            <button   class="btn btn-info  mx-3" onClick={Lock}>{locktext} <i class="fas fa-lock"></i></button><br/>
+       
+      
         </div>
+        </>
     )
 }
 
