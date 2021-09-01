740279490e8e41a68acdcfc0f7b9434e0d0e3c1c
AREEJ AMJAD <56145161+areej14@users.noreply.github.com>
2021-09-01 23:24:32 +0500

    slider

--- react-application/src/App.js
+++ react-application/src/App.js
@@ -1,16 +1,12 @@
-import React from "react";
-import "./App.css";
-import Header from "./Components/Header"
-import Cardmap from "./Components/Cardmap"
+import React from 'react'
+import Slider from './Components/Slider'
 
 function App() {
- 
   return (
-    <>
-     {Header}
-     <Cardmap/>
-    </>
-  );
+    <div>
+      <Slider/>
+    </div>
+  )
 }
 
-export default App;
+export default App
