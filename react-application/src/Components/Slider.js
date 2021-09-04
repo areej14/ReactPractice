3db0aa08f169ae38bbdb21aa62fc1c1e476fb571
AREEJ AMJAD <56145161+areej14@users.noreply.github.com>
2021-09-04 15:28:25 +0500

    commit

--- react-application/src/Components/Slider.js
+++ react-application/src/Components/Slider.js
@@ -3,10 +3,10 @@ function Slider() {
   const array=["http://cdn.mos.cms.futurecdn.net/6t8Zh249QiFmVnkQdCCtHK.jpg","https://cdn.mos.cms.futurecdn.net/k9Md6R78D8aN4tbGDRWUSE.jpg","https://i.insider.com/58287139691e8849528b5fe3?width=1000&format=jpeg&auto=webp","https://5.imimg.com/data5/XC/NP/NL/SELLER-874584/dell-xps-13-9370-500x500.jpg","https://cdn.vox-cdn.com/thumbor/lRwetR_dg8WBLFIUPzY7l0QYCaI=/1400x0/filters:no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/22411713/cfaulkner_20210326_4491_0006.jpg"]
   const [i, seti] = useState(0)
   const Next=()=>{
-   (i>=0 && i< array.length-1)?seti(i+1):alert("You are on last image")
+   (i>=0 && i< array.length-1)?seti(i+1):seti(0)
 }
   const Pre=()=>{
-    (i===0)?alert("You are on first image"):seti(i-1)
+    (i===0)?seti(4):seti(i-1)
  }
   return (
     <>
