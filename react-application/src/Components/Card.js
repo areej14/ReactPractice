eb71cb53f53c60a76ecd13281978e5c362a17631
AREEJ AMJAD <56145161+areej14@users.noreply.github.com>
2021-09-06 21:37:13 +0500

    class12

--- react-application/src/Components/Card.js
+++ react-application/src/Components/Card.js
@@ -2,33 +2,34 @@ import React from 'react'
 import "../App.css"
 const Card = (props) => {
 
-   const {items :{title,src,price,content}} = props;
-   return (
-       <>
-      
-         <div>
-          
-           <div  className="card cardb" style={{"width": "18rem"}}>
-           <h4>  {title}</h4>
-         <img className="card-img-top image" src={src} alt="Card  cap" />
-         <div className="card-body">
-           <h5 className="card-title"><mark><b>Price: </b>{price} $</mark></h5><hr/><hr/>
-           <p className="card-text"><mark><b> {content}</b></mark></p>
-         </div>
-         </div>
-      
-       </div>
-      
-       </>
-       
+  const { items: { title, img, category, price, desc }, index } = props;
 
 
-     
-     
-     
- )
-          }
-       
-   
+  return (
+    <>
+
+      <div>
+        <div className="card cardb" style={{ "width": "18rem" }}>
+          <h4>{index + 1}. {title}</h4>
+          <img className="card-img-top image" src={img} alt="Card  cap" />
+          <div className="card-body">
+            <h5 className="card-title"><b>Price: </b>{price} $</h5>
+            <p className="card-text"><b> {category}</b><br />{desc}</p>
+          </div>
+        </div>
+
+      </div>
+
+    </>
+
+
+
+
+
+
+  )
+}
+
+
 
 export default Card
--- react-application/src/Components/Cardmap.js
+++ react-application/src/Components/Cardmap.js
@@ -3,21 +3,32 @@ import List from "./newData.jsx";
 import "../App.css";
 import Card from "./Card";
 const Cardmap = () => {
-  const [data, setdata] = useState([]);
+  const [data, setdata] = useState(List);
+
+  const filter = (value) => {
+    if (value !== "All") {
+      const newlist = List.filter((item) => item.category === value)
+      setdata(newlist)
+    }
+    else {
+      setdata(List)
+    }
+  }
   return (
     <>
-      <h2>TOTAL IMAGES {data.length}</h2>
-      
-      <div className="body">
-      {/* {data.filter((it,i)=>( it.title=="Nike Shoes 02")).map((items)=>(<Card items={items}/>))} */}
-        {data.map((item, i) => ( <Card items={item} key={i} /> ))}
-        
-      </div>
+      <h2>Menu Card</h2>
       <div className="btns">
-        <button className="btn btn-success mx-2"onClick={() => {setdata(List)}}>Show</button>
-        <button
-          className="btn btn-danger"onClick={() => {setdata([]); }}>Clear</button>
+        <button className="btn btn-success  mx-2" onClick={() => { filter('All') }}>All</button>
+        <button className="btn btn-warning text-white mx-2" onClick={() => { filter('Shakes'); }}>Shake</button>
+        <button className="btn btn-primary mx-2" onClick={() => { filter('Breakfast') }}>Breakfast</button>
+        <button className="btn btn-info mx-2" onClick={() => { filter('Lunch'); }}>Lunch</button>
+        <button className="btn btn-danger mx-2" onClick={() => { setdata([]) }}>Clear</button>
       </div>
+
+      <div className="body">
+        {data.map((item, i) => (<Card items={item} index={i} key={i} />))}
+      </div>
+
     </>
   );
 };
