c27518d1d6e05fdcd903bd241cb678578de20592
AREEJ AMJAD <56145161+areej14@users.noreply.github.com>
2021-09-12 21:25:39 +0500

    commit

---
+++ react-application/src/Components/Task.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Task({todo,i,edit,del}) {
+    console.log(todo);
+    return (
+        <div>
+            <h4>{todo.id = i + 1}. {todo.task} 
+                    <button className="btn btn-outline-warning ml-5 mx-1 my-1" type="button" onClick={() => del(i + 1)} > <i className="fas fa-times"></i></button>
+                    <button className="btn btn-outline-light mx-1 my-1" type="button" onClick={() => edit(todo.task, i + 1)} ><i className="fas fa-pen"></i></button>
+                </h4>
+        </div>
+    )
+}
+
+export default Task
--- react-application/src/Components/Todo.jsx
+++ react-application/src/Components/Todo.jsx
@@ -1,63 +1,57 @@
 import React, { useState } from 'react'
+import Task from './Task'
+
 export default function Todo() {
+
     const [list, setlist] = useState([])
     const [text, settext] = useState('')
+
     const TextOnChange = (e) => {
-
         settext(e.target.value)
-
     }
+
     const additem = () => {
-
-        setlist([...list, {task:text ,id:''} ])
+        setlist([...list, { task: text, id: null }])
         localStorage.setItem('items', JSON.stringify(list));
-        console.log(list);
+       
         settext('')
-
-
     }
+
     const delAll = () => {
         setlist([])
         localStorage.clear();
     }
-    const delitem = (deltask) => {
 
-        setlist(list.filter((todoo) => (deltask !== todoo.id)))
-         }
-    
-         const edititem = (edit,index)=> {
-          const task= prompt("edit this task",edit)
-        //   console.log(edit,task);
-         const newlist=list.map(editlist=>{ if(index===editlist.id)
-            {
-             editlist.task = task
-             console.log(index,editlist.task,editlist.id)
-         }
-        
-         })
-         setlist(newlist)
-        
-        
-        }
-    
+    const delitem = (deltask) => {
+        setlist(list.filter((del) => (deltask !== del.id)))
+    }
+
+    const edititem = (value, index) => {
+        const updated = prompt("edit this task", value)
+
+        list.map((editlist) => {
+            if (index === editlist.id) {
+                editlist.task = updated;
+
+            }})
+
+        setlist([...list])
+    }
+
     return (
-        <div  className="container my-2" >
+        <div className="container my-2" >
 
             <h2 style={{ textAlign: 'center' }} > <i className="fas fa-tasks"> My Todo App</i></h2>
             <div className="input-group" style={{ textAlign: 'center', width: '70%', margin: '0 auto' }}>
-                <input style={{ backgroundColor: 'rgb(170, 170, 223)' }} type="text" onChange={TextOnChange} className="form-control" value={text} placeholder="Enter Your Task" aria-label="Recipient's username with two button addons" />
-                <button className="btn btn-outline-success" type="button" onClick={() => additem()}><i className="fas fa-plus-square"></i></button>
+                <input style={{ backgroundColor: 'rgb(170, 170, 223)' }} type="text" onChange={TextOnChange} className="form-control" value={text} placeholder="Enter Your Task"  />
+                <button className="btn btn-outline-success mx-1" type="button" onClick={() => additem()}><i className="fas fa-plus-square"></i></button>
                 <button className="btn btn-outline-danger" type="button" onClick={() => delAll()}><i className="fas fa-trash-alt"></i></button>
             </div>
 
-            {list.map((todo, i) => <div style={{ marginLeft: '16%', marginTop: '10px' }} key={i}>
-                <h3>{todo.id=i+1} {todo.task}
-                <button className="btn btn-info mx-2 my-1" onClick={() => delitem(i+1)} ><i className="fas fa-minus-square"></i></button> 
-                   
-                    <button className="btn btn-info mx-2 my-1" onClick={() => edititem(todo.task,i+1)} ><i className="fas fa-link"></i></button>
-                </h3>
+            {list.map((data, index) => <div style={{ marginLeft: '18%', marginTop: '14px' }} key={index}>
+                <Task todo={data} i={index} edit={edititem} del={ delitem}/>
             </div>)}
 
         </div>
     )
-    }
+}
