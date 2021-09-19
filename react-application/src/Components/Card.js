1ced7327f9f6d42c7f170432efb1580c901c555e
AREEJ AMJAD <56145161+areej14@users.noreply.github.com>
2021-09-19 17:55:58 +0500

    added

--- react-application/src/Components/Card.js
+++ react-application/src/Components/Card.js
@@ -17,8 +17,8 @@ const Card = ({data,fun}) => {
           <img className="card-img-top image" src={data.image} alt="Card  cap" />
           <div className="card-body">
             <h5 className="card-title"><mark><b>Price: </b>{data.price}$</mark></h5>
-            <p className="card-text">{Readmore === data.id ? data.info : (data.info.slice(0, 27))}
-              <span onClick={() => show(data.id)}>{Readmore === data.id ? ( <button className="btn btn-info mx-2" style={{outline:'none', border: 'none'}}> Read Less </button>):(<button  className="btn btn-info mx-2" style={{outline:'none', border: 'none'}}> Read More </button>)}</span>
+            <p className="card-text">{Readmore === data.id ? data.info : (data.info.slice(0, 87))}
+              <span onClick={() => show(data.id)}>{Readmore === data.id ? ( <button className="btn btn-info btn-sm mx-2" style={{outline:'none', border: 'none'}}> Read Less </button>):(<button  className="btn btn-sm btn-info mx-2" style={{outline:'none', border: 'none'}}> Read More </button>)}</span>
             </p>
             <button className="btn btn-danger" onClick={() => fun(data.id)}>Not Interested</button>
           </div>
