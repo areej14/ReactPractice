11ce0ff3a786b806b8181bf231d411c7f205718f
AREEJ AMJAD <56145161+areej14@users.noreply.github.com>
2021-09-25 21:44:21 +0500

    done

--- react-application/src/Components/Cardmap.js
+++ react-application/src/Components/Cardmap.js
@@ -1,26 +1,26 @@
 import React, { useEffect, useState } from 'react'
 import "../App.css"
 import Details from './Details'
-import Loader from './Loader'
+
 
 const Cardmap = () => {
     const [input, setinput] = useState([])
     const [data, setdata] = useState([])
-    const [value, setvalue] = useState('')
-    const [name, setname] = useState('')
-   
-    
+    const [value, setvalue] = useState()
+    const [name, setname] = useState()
+
+
 
 
     const headers = { AUTHORIZATION: "Token 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" }
     async function Api() {
         try {
             let Api = "https://api.github.com/users";
-            let url = await fetch((Api) , { "method": "GET", "headers": headers })
+            let url = await fetch((Api), { "method": "GET", "headers": headers })
             let parsedData = await url.json();
             setinput(parsedData)
             setdata(parsedData)
-          
+
         } catch (error) {
             alert("Error: " + error.message)
         }
@@ -30,50 +30,42 @@ const Cardmap = () => {
     }
 
     const Check = (e) => {
-
         setvalue(e.target.value)
         setinput(data.filter((item) => (item.login.toLowerCase().includes(e.target.value.toLowerCase()))));
     }
     async function User(e) {
-     
+
         setname(e.target.value)
-        const url = await fetch((`https://api.github.com/users/${name}`), { "method": "GET", "headers": headers })
+        const url = await fetch((`https://api.github.com/users/${e.target.value}`), { "method": "GET", "headers": headers })
         let parsedData = await url.json();
-        try{
-        e.target.value===''?setinput(data):
-        setinput([parsedData])
-      }
-      catch (error) {
-          alert('Error',error.message)
-      }
-        
+        ((e.target.value === '') || (parsedData.message === 'Not Found')) ? setinput(data) :
+            setinput([parsedData])
+
+
 
     }
     useEffect(() => {
-        Api()
+        Api();
 
     }, [])
 
     return (
         <>
-            
-
-               <div style={{textAlign:'center' }}>
-                   <h2>Search Github Users <i class="fas fa-users"></i></h2>
-                   
-                  <input value={value} className="my-2"onChange={(value) => Check(value)} type="text" placeholder="Search from List"/> <i className="fas fa-search"></i><br/>
-                  <input value={name} onChange={(name) => User(name)} type="text" placeholder="Search Other Users"/> <i className="fas fa-search"></i> <br/><br/>
 
 
+            <div style={{ textAlign: 'center' }}>
+                <h2>Search Github Users <i className="fas fa-users"></i></h2>
 
-                   
-                    <div className="row" >
-                   
-                    { input.map((item, i) => <Details data={item} key={i}  />)}
-               
+                <input value={value} className="my-2" onChange={Check} type="text" placeholder="--Search from List--" /> <i className="fas fa-search"></i><br />
+                <input value={name} onChange={User}  type="text" placeholder="--Search Other Users--" /> <i className="fas fa-search"></i> <br /><br />
+
+                <div className="row" >
+
+                    {input.map((item, i) => <Details data={item} key={i} />)}
+
                 </div>
-                </div>
-               
+            </div>
+
 
         </>
     )
--- react-application/src/Components/Details.js
+++ react-application/src/Components/Details.js
@@ -2,29 +2,29 @@ import React from 'react'
 function Details({ data }) {
 
   return (
-    
+
     <div className=" col-md-6" >
-    <div style={{ fontFamily: "cursive" }}>
+      <div style={{ fontFamily: "cursive" }}>
 
 
-<div className="card mb-3" style={{maxWidth: "540px"}}>
-  <div className="row g-0">
-    <div className="col-md-4">
-      <img src={data.avatar_url} style={{height: "240px"}}className="img-fluid rounded-start" alt="..."/>
-    </div>
-    <div className="col-md-8">
-      <div className="card-body">
-        <h5 className="card-title"> {data.login}</h5>
-       <h5>{data.type}</h5>
-       
-      <p>Hi... I am a github User</p>
-        <a href={data.repos_url} target="_blank"><button className="btn btn-outline-primary mx-2">Repositories</button></a>
-        <a href={data.html_url} target="_blank"><button className="btn btn-outline-primary"> Github Account</button></a>
-     </div>
-    </div>
-  </div>
-</div>
-</div>
+        <div className="card mb-3" style={{ maxWidth: "540px" }}>
+          <div className="row g-0">
+            <div className="col-md-4">
+              <img src={data.avatar_url} style={{ height: "240px" }} className="img-fluid rounded-start" alt="..." />
+            </div>
+            <div className="col-md-8">
+              <div className="card-body">
+                <h5 className="card-title"> {data.login}</h5>
+                <h5>{data.type}</h5>
+
+                <p>Hi... I am a github User</p>
+                <a href={data.repos_url} rel="noreferrer" target="_blank"><button className="btn btn-outline-primary mx-2">Repositories</button></a>
+                <a href={data.html_url}  rel="noreferrer" target="_blank"><button className="btn btn-outline-primary"> Github Account</button></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
