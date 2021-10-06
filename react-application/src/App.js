a412934ca8b28541e8d9bd1db709d85113c272a6
AREEJ AMJAD <56145161+areej14@users.noreply.github.com>
2021-10-06 09:16:32 +0500

    done

--- react-application/src/App.js
+++ react-application/src/App.js
@@ -1,4 +1,4 @@
-import {Route} from "react-router-dom";
+import {Route, Switch} from "react-router-dom";
 import NavBbar from "./Components/NavBbar";
 import All from './Components/All';
 import Shake from './Components/Shake'
@@ -11,10 +11,12 @@ function App() {
     <NavBbar/>
           {/* <Route path="/" exact = {true}><Home/><h3>My best Page</h3></Route>       //note you can write any html here,,,  or component
           <Route path="/about"><About/></Route> */}
-          <Route path="/" exact = {true} className="btn btn-success  mx-2" ><All /></Route>
+          <Switch>
+          <Route path="/" exact={true} className="btn btn-success  mx-2" ><All /></Route>
         <Route path="/Shake"className="btn btn-warning text-white mx-2" ><Shake/></Route>
         <Route path="/Breakfast"className="btn btn-primary mx-2" ><Breakfast /></Route>
         <Route path="/Lunch"className="btn btn-info mx-2" ><Lunch /></Route>
+        </Switch>
     </>
   );
 }
