a160a63fbeb26a19633adb9526752177fcda6214
AREEJ AMJAD <56145161+areej14@users.noreply.github.com>
2021-10-08 17:51:46 +0500

    /

--- react-application/src/Components/NavBbar.js
+++ react-application/src/Components/NavBbar.js
@@ -1,19 +1,19 @@
 import React from 'react'
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function NavBbar() {
-    return (
-        <>
-        <h2>Menu Card</h2>
-        <div className="btns">
+  return (
+    <>
+      <h2>Menu Card</h2>
+      <div className="btns">
         <Link to="/" className="btn btn-success  mx-2" >All</Link>
-        <Link to="/Shake"className="btn btn-warning text-white mx-2" >Shake</Link>
-        <Link to="/Breakfast"className="btn btn-primary mx-2" >Breakfast</Link>
-        <Link to="/Lunch"className="btn btn-info mx-2" >Lunch </Link>
-      
+        <Link to="/Shake" className="btn btn-warning text-white mx-2" >Shake</Link>
+        <Link to="/Breakfast" className="btn btn-primary mx-2" >Breakfast</Link>
+        <Link to="/Lunch" className="btn btn-info mx-2" >Lunch </Link>
+
       </div>
-      </>
-    )
+    </>
+  )
 }
 
 export default NavBbar
