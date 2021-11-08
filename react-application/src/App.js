a1a34729a18206c0fd3340b7d83b9aa72c5bc03e
AREEJ AMJAD <56145161+areej14@users.noreply.github.com>
2021-11-08 18:39:50 +0500

    .

--- react-application/src/App.js
+++ react-application/src/App.js
@@ -9,6 +9,7 @@ function App() {
   const webcamRef = useRef(null)
   const canvasRef = useRef(null)
   const connect = window.drawConnectors;
+  console.log('hh',connect);
   var camera=null;
   function onResults(results) {
     // const video = webcamRef.current.video;
