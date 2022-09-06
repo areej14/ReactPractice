f849e7c97dbaf5a214be72cbcbcbf31cd4c00106
Junaid Butt <areej.amjad14@gmail.com>
2022-09-06 09:24:04 +0500

    .

--- react-application/src/App.js
+++ react-application/src/App.js
@@ -1,7 +1,19 @@
-
+import {useEffect} from 'react'
 import './App.css';
 
 function App() {
+  function Data() {
+   
+    
+    fetch('https://data.covid19india.org/data.json')
+      .then(response => response.json())
+      .then(response => console.log(response))
+      .catch(err => console.error(err));
+  }
+  useEffect(() => {
+   Data();
+    }, []);
+  
   return (
     <>
     </>
